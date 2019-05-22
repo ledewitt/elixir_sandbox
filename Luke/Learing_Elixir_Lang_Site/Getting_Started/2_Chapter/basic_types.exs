05e415af4264a30787e42b458cc39d304c7ed4d7
Luke DeWitt <luke@dewittsoft.com>
2019-05-21 19:44:50 -0600

    Starting out

---
+++ Luke/Learing_Elixir_Lang_Site/Getting_Started/2_Chapter/basic_types.exs
@@ -0,0 +1,15 @@
+IO.puts "Playing with basic types"
+
+IO.puts "Some Arithmetic"
+
+IO.puts "1 + 2 =" 
+IO.puts 1+2
+
+IO.puts "5 * 5"
+IO.puts 5*5
+
+IO.puts "10 / 2"
+IO.puts 10/2
+
+IO.puts ""
+IO.puts "Dividing Stuff"
---
+++ Luke/Learing_Elixir_Lang_Site/Rabbit_Holes/power.exs
@@ -0,0 +1,7 @@
+defmodule Power do
+    def power(x,n) do
+        result = x
+        Enum.each(1..n, result = x * result ->
+             )
+    end
+end
